2b8f033292b5e9baeea750c459f33d93b31a0426
YakshJakharia06 <jakhariayaksh@gmail.com>
2026-01-20 12:56:53 +0530

    Update page.js

--- app/[shorturl]/page.js
+++ app/[shorturl]/page.js
@@ -2,26 +2,27 @@ import { redirect } from "next/navigation"
 import clientPromise from "@/lib/mongodb"
 
 export default async function Page({ params }) {
+	
+    const resolvedParams = await params;
+    const { shorturl } = resolvedParams;
+    const client = await clientPromise;
+    const db = client.db("bitLinks");
+    const collection = db.collection("url");
+    const result = await collection.findOneAndUpdate(
+        { shorturl: shorturl },
+        { $inc: { visits: 1 } },
+        { returnDocument: 'after' }
+    );
 
-	const { shorturl } = await params
-	const client = await clientPromise;
-	const db = client.db("bitLinks")
-	const collection = db.collection("url")
-	const doc = await collection.findOneAndUpdate(
-		{ shorturl: shorturl },
-		{ $inc: { visits: 1 } },
-		{ returnDocument: 'after' }
-	);
-	console.log(doc);
+    const doc = result?.value || result;
 
-	// if document exists redirect to page
-	if (doc) {
-		const destination = doc.url.startsWith("http") ? doc.url : `https://${doc.url}`;
-		redirect(destination);
-	} else {
-		redirect(process.env.NEXT_PUBLIC_HOST)
-	}
-
-
-	// return <div>My Post: {url}</div>
+    if (doc && doc.url) {
+        const destination = doc.url.startsWith("http") 
+            ? doc.url 
+            : `https://${doc.url}`;
+        
+        return redirect(destination);
+    } else {
+        return redirect(process.env.NEXT_PUBLIC_HOST || "/");
+    }
 }
